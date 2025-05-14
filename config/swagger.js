200330e211ec61f06bf1a1757df066431b3b8722
totrieuvy <vytotrieu12a121@gmail.com>
2025-05-14 20:28:07 +0700

    chỉnh lại link url deploy

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: process.env.SWAGGER_SERVER_URL || "http://localhost:3000",
       },
     ],
     components: {
