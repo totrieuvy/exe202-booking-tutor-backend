7c50afde0f22b2ead7ec0cef32b4e1a95bbc3fb9
totrieuvy <vytotrieu12a121@gmail.com>
2025-05-14 21:17:05 +0700

    Update Swagger server URL for Render

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,8 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        // url: "http://localhost:3000",
+        url: "https://exe202-booking-tutor-backend.onrender.com",
       },
     ],
     components: {
